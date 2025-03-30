bb77b452c1be4a78f50198750ff05ac3d635d1b1
yzq0325 <ziqi.yang@uzh.ch>
2025-03-30 17:52:37 +0200

    [task #11, #12] fix metadata issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,13 +15,11 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+export { metadata };
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ConfigProvider
           theme={{
