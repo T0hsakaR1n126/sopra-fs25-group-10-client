a36e75c6189f55159f28f788f591e866f77772bf
yzq0325 <ziqi.yang@uzh.ch>
2025-03-30 17:38:34 +0200

    [task #11, #12] home page enhancement

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,18 +1,11 @@
 "use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 
 import React, { useState } from "react";
-import useLocalStorage from "@/hooks/useLocalStorage";
 import "../styles/dashboard.css";
 import { useRouter } from "next/navigation"; // use NextJS router for navigation
 
 const Dashboard: React.FC = () => {
   const router = useRouter();
-  const {
-    // value: token, // is commented out because we dont need to know the token value for logout
-    // set: setToken, // is commented out because we dont need to set or update the token value
-    clear: clearToken, // all we need in this scenario is a method to clear the token
-  } = useLocalStorage<string>("token", ""); // if you wanted to select a different token, i.e "lobby", useLocalStorage<string>("lobby", "");
-
   const [selectedSoloTime, setSelectedSoloTime] = useState("5");
   const [showSoloPopup, setSoloShowPopup] = useState(false);
 
