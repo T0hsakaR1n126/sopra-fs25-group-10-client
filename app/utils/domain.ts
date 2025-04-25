d2dcb6c5be8915cbd71632bf17a13aa8a6e49ca3
yzq0325 <ziqi.yang@uzh.ch>
2025-04-25 23:49:07 +0200

    [fix] domain config

--- app/utils/domain.ts
+++ app/utils/domain.ts
@@ -7,9 +7,9 @@ import { isProduction } from "@/utils/environment";
  */
 export function getApiDomain(): string {
   const prodUrl = process.env.NEXT_PUBLIC_PROD_API_URL ||
-    // "https://sopra-fs25-group-10-server-246820907268.europe-west6.run.app"; // TODO: update with your production URL as needed.
-    "http://localhost:8080"; // TODO: update with your production URL as needed.
-  // const devUrl = "https://sopra-fs25-group-10-server-246820907268.europe-west6.run.app";
-  const devUrl = "http://localhost:8080"; // TODO: update with your development URL as needed.
+    "https://sopra-fs25-group-10-server-246820907268.europe-west6.run.app"; // TODO: update with your production URL as needed.
+    // "http://localhost:8080"; // TODO: update with your production URL as needed.
+  const devUrl = "https://sopra-fs25-group-10-server-246820907268.europe-west6.run.app";
+  // const devUrl = "http://localhost:8080"; // TODO: update with your development URL as needed.
   return isProduction() ? prodUrl : devUrl;
 }
