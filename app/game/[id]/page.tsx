c0b32c4b462cddf2852f98bc2d84324107f2a434
yzq0325 <ziqi.yang@uzh.ch>
2025-04-25 16:44:02 +0200

    [dev] websocket with cloud run config

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -66,7 +66,7 @@ const GameBoard: React.FC = () => {
     }
 
     const client = new Client({
-      brokerURL: 'http://localhost:8080/ws', // TODO: replace with your WebSocket URL
+      brokerURL: 'wss://sopra-fs25-group-10-server-246820907268.europe-west6.run.app/ws', // TODO: replace with your WebSocket URL
       reconnectDelay: 5000,
       onConnect: () => {
         console.log('STOMP connected');
