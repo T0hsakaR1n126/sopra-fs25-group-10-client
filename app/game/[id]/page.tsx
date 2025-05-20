41b9bbee902e41d5d8d86134e1a7f5d6691fab8d
Zheyuan Fu <accelerator0126@gmail.com>
2025-05-21 01:09:56 +0200

    #97 fix some bugs

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -100,8 +100,8 @@ const GameBoard: React.FC = () => {
     }
 
     const client = new Client({
-      brokerURL: 'wss://sopra-fs25-group-10-server.oa.r.appspot.com/ws', // TODO: replace with your WebSocket URL
-      // brokerURL: "http://localhost:8080/ws",
+      // brokerURL: 'wss://sopra-fs25-group-10-server.oa.r.appspot.com/ws', // TODO: replace with your WebSocket URL
+      brokerURL: "http://localhost:8080/ws",
       reconnectDelay: 5000,
       onConnect: () => {
         console.log('STOMP connected');
